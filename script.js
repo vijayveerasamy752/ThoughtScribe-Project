76eecb2ac63670cb53d0dce92f7204133619dade
VijayVeerasamy <vijayveerasamy752@gmail.com>
2024-11-09 17:52:34 +0530

    Build Design

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-//selecting buttton,popupbox,popupoverley
-var addbutton=document.getElementById("add-popup-button")
-var popupoverley=document.querySelector(".popup-overley")
-var popupbox=document.querySelector(".popup-box")
-addbutton.addEventListener("click",function(){
-popupoverley.style.display="block"
-popupbox.style.display="block"
-})
-//select cancel button
-var cancel=document.getElementById("cancel-story")
-cancel.addEventListener("click",function(event){
-    event.preventDefault()
-    popupoverley.style.display="none"
-    popupbox.style.display="none"
-})
-// selecting container,add-story,story-input,author-input,
-var container=document.querySelector(".container")
-var addstory=document.getElementById("add-story")
-var storyinput=document.getElementById("story-input")
-var authorinput=document.getElementById("author-input")
-var storydes=document.getElementById("story-des")
-addstory.addEventListener("click",function(event){
-    event.preventDefault()
-    var div=document.createElement("div")
-    div.setAttribute("class","book-container")
-    div.innerHTML=`<h2>${storyinput.value}</h2>  
-    <h5>${authorinput.value}</h5>
-            <p>${storydes.value}</p>
-            <button onclick="deletestory(event)">Delete</button>`
+// Selecting elements
+var addButton = document.getElementById("add-popup-button");
+var popupOverlay = document.querySelector(".popup-overlay");
+var popupBox = document.querySelector(".popup-box");
 
-            container.append(div)
-             popupoverley.style.display="none"
-             popupbox.style.display="none"
-})
-function deletestory(event){
-event.target.parentElement.remove()
+// Show popup
+addButton.addEventListener("click", function() {
+    popupOverlay.style.display = "block";
+    popupBox.style.display = "block";
+});
+
+// Cancel popup
+var cancel = document.getElementById("cancel-story");
+cancel.addEventListener("click", function(event) {
+    event.preventDefault();
+    popupOverlay.style.display = "none";
+    popupBox.style.display = "none";
+});
+
+// Add story
+var container = document.querySelector(".container");
+var addStory = document.getElementById("add-story");
+var storyInput = document.getElementById("story-input");
+var authorInput = document.getElementById("author-input");
+var storyDes = document.getElementById("story-des");
+
+addStory.addEventListener("click", function(event) {
+    event.preventDefault();
+    var div = document.createElement("div");
+    div.setAttribute("class", "book-container");
+    div.innerHTML = `
+        <h2>${storyInput.value}</h2>
+        <h5>${authorInput.value}</h5>
+        <p>${storyDes.value}</p>
+        <button onclick="deleteStory(event)">Delete</button>`;
+    
+    container.append(div);
+
+    // Reset form and hide popup
+    storyInput.value = "";
+    authorInput.value = "";
+    storyDes.value = "";
+    popupOverlay.style.display = "none";
+    popupBox.style.display = "none";
+});
+
+// Delete story
+function deleteStory(event) {
+    event.target.parentElement.remove();
 }
